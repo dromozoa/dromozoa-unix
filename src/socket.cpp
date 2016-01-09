f6cac7ab1f1adbb5afe0d8e0e7cf51e7f275c646
Tomoyuki Fujimori <moyu@dromozoa.com>
2016-01-09 11:38:47 +0900

    socketpairを追加。

--- src/socket.cpp
+++ src/socket.cpp
@@ -21,11 +21,32 @@ extern "C" {
 
 #include <sys/socket.h>
 
+#include "error.hpp"
+#include "fd.hpp"
+#include "function.hpp"
 #include "set_field.hpp"
 #include "socket.hpp"
 
 namespace dromozoa {
+  namespace {
+    int impl_socketpair(lua_State* L) {
+      int domain = luaL_checkinteger(L, 1);
+      int type = luaL_checkinteger(L, 2);
+      int protocol = luaL_optinteger(L, 3, 0);
+      int fd[2] = { -1, -1 };
+      if (socketpair(domain, type, protocol, fd) == -1) {
+        return push_error(L);
+      } else {
+        new_fd(L, fd[0]);
+        new_fd(L, fd[1]);
+        return 2;
+      }
+    }
+  }
+
   void initialize_socket(lua_State* L) {
+    function<impl_socketpair>::set_field(L, "socketpair");
+
     DROMOZOA_SET_FIELD(L, AF_INET);
     DROMOZOA_SET_FIELD(L, AF_INET6);
     DROMOZOA_SET_FIELD(L, AF_UNIX);
