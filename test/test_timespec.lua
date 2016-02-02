52982b7ab2e09f5ca941b2c033e694a6e5fa952b
Tomoyuki Fujimori <moyu@dromozoa.com>
2016-02-03 08:56:13 +0900

    メタメソッドを追加。

--- test/test_timespec.lua
+++ test/test_timespec.lua
@@ -32,3 +32,8 @@ local t = t2 - t1
 assert(t.tv_sec == 0)
 assert(100000000 < t.tv_nsec and t.tv_nsec < 300000000)
 assert(t1 + t == t2)
+
+local t1 = unix.timespec({ tv_sec = 1, tv_nsec = 0 })
+local t2 = unix.timespec({ tv_sec = 1, tv_nsec = 1 })
+assert(t1 <= t1)
+assert(t1 <= t2)
