25a7117ddfc31024b3bf60335ed2f67878cc7b8f
Tomoyuki Fujimori <moyu@dromozoa.com>
2016-04-14 15:16:32 +0900

    ファイルを分割。

--- src/lua/timespec.cpp
+++ src/lua/timespec.cpp
@@ -15,26 +15,28 @@
 // You should have received a copy of the GNU General Public License
 // along with dromozoa-unix.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <math.h>
+
 #include "common.hpp"
 
 namespace dromozoa {
-  namespace {
-    void impl_nanosleep(lua_State* L) {
-      struct timespec tv1 = {};
-      struct timespec tv2 = {};
-      check_timespec(L, 1, tv1);
-      if (nanosleep(&tv1, &tv2) != -1) {
-        luaX_push_success(L);
-      } else {
-        push_error(L);
-        lua_newtable(L);
-        luaX_set_field(L, "tv_sec", tv2.tv_sec);
-        luaX_set_field(L, "tv_nsec", tv2.tv_nsec);
-      }
+  bool check_timespec(lua_State* L, int n, struct timespec& tv) {
+    if (lua_isnoneornil(L, n)) {
+      return false;
+    } else if (lua_isnumber(L, n)) {
+      double t = lua_tonumber(L, n);
+      double i = 0;
+      double f = modf(t, &i);
+      tv.tv_sec = i;
+      tv.tv_nsec = f * 1000000000;
+      return true;
+    } else if (lua_istable(L, n)) {
+      tv.tv_sec = luaX_opt_integer_field<time_t>(L, n, "tv_sec", 0);
+      tv.tv_nsec = luaX_opt_integer_field<long>(L, n, "tv_nsec", 0, 0L, 999999999L);
+      return true;
+    } else {
+      luaL_argerror(L, n, "nil, number or table expected");
+      return false;
     }
-  }
-
-  void initialize_time(lua_State* L) {
-    luaX_set_field(L, "nanosleep", impl_nanosleep);
   }
 }
