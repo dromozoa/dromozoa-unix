c54db01bea1d5886711296b7dfae1bde80fd121e
Tomoyuki Fujimori <moyu@dromozoa.com>
2016-05-11 08:34:24 +0900

    timerを追加。

---
+++ include/dromozoa/timer_mach.hpp
@@ -0,0 +1,35 @@
+// Copyright (C) 2016 Tomoyuki Fujimori <moyu@dromozoa.com>
+//
+// This file is part of dromozoa-unix.
+//
+// dromozoa-unix is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// dromozoa-unix is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with dromozoa-unix.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef DROMOZOA_TIMER_MACH_ABSOLUTE_TIME_HPP
+#define DROMOZOA_TIMER_MACH_ABSOLUTE_TIME_HPP
+
+namespace dromozoa {
+  class timer_mach_absolute_time {
+  public:
+    timer_mach_absolute_time();
+    virtual ~timer_mach_absolute_time();
+    virtual int start();
+    virtual int stop();
+    virtual double elapsed();
+  private:
+    uint64_t start_;
+    uint64_t stop_;
+  };
+}
+
+#endif
