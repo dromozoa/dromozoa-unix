10f85a21b89be9dd625de734ce044c85354fb7e8
Tomoyuki Fujimori <moyu@dromozoa.com>
2019-01-24 09:00:57 +0900

    syslogを修正。

--- src/lua/syslog.cpp
+++ src/lua/syslog.cpp
@@ -21,19 +21,71 @@
 
 namespace dromozoa {
   namespace {
+    std::string syslog_ident;
+
     void impl_openlog(lua_State* L) {
+      luaX_string_reference ident = luaX_to_string(L, 1);
+      int option = luaX_opt_integer<int>(L, 2, 0);
+      int facility = luaX_opt_integer<int>(L, 3, LOG_USER);
+      if (ident) {
+        syslog_ident.assign(ident.data(), ident.size());
+        openlog(syslog_ident.c_str(), option, facility);
+      } else {
+        openlog(0, option, facility);
+      }
+      luaX_push_success(L);
     }
 
     void impl_syslog(lua_State* L) {
+      int priority = luaX_check_integer<int>(L, 1);
+      luaX_string_reference message = luaX_check_string(L, 2);
+      syslog(priority, "%s", message.data());
+      luaX_push_success(L);
     }
 
     void impl_closelog(lua_State* L) {
+      closelog();
+      syslog_ident.clear();
+      luaX_push_success(L);
     }
   }
 
-  void initialize_sys_socket(lua_State* L) {
+  void initialize_syslog(lua_State* L) {
     luaX_set_field(L, -1, "openlog", impl_openlog);
     luaX_set_field(L, -1, "syslog", impl_syslog);
     luaX_set_field(L, -1, "closelog", impl_closelog);
+
+    luaX_set_field(L, -1, "LOG_PID", LOG_PID);
+    luaX_set_field(L, -1, "LOG_CONS", LOG_CONS);
+    luaX_set_field(L, -1, "LOG_NDELAY", LOG_NDELAY);
+    luaX_set_field(L, -1, "LOG_ODELAY", LOG_ODELAY);
+    luaX_set_field(L, -1, "LOG_NOWAIT", LOG_NOWAIT);
+
+    luaX_set_field(L, -1, "LOG_KERN", LOG_KERN);
+    luaX_set_field(L, -1, "LOG_USER", LOG_USER);
+    luaX_set_field(L, -1, "LOG_MAIL", LOG_MAIL);
+    luaX_set_field(L, -1, "LOG_NEWS", LOG_NEWS);
+    luaX_set_field(L, -1, "LOG_UUCP", LOG_UUCP);
+    luaX_set_field(L, -1, "LOG_DAEMON", LOG_DAEMON);
+    luaX_set_field(L, -1, "LOG_AUTH", LOG_AUTH);
+    luaX_set_field(L, -1, "LOG_CRON", LOG_CRON);
+    luaX_set_field(L, -1, "LOG_LPR", LOG_LPR);
+    luaX_set_field(L, -1, "LOG_LOCAL0", LOG_LOCAL0);
+    luaX_set_field(L, -1, "LOG_LOCAL1", LOG_LOCAL1);
+    luaX_set_field(L, -1, "LOG_LOCAL2", LOG_LOCAL2);
+    luaX_set_field(L, -1, "LOG_LOCAL3", LOG_LOCAL3);
+    luaX_set_field(L, -1, "LOG_LOCAL4", LOG_LOCAL4);
+    luaX_set_field(L, -1, "LOG_LOCAL5", LOG_LOCAL5);
+    luaX_set_field(L, -1, "LOG_LOCAL6", LOG_LOCAL6);
+    luaX_set_field(L, -1, "LOG_LOCAL7", LOG_LOCAL7);
+
+    luaX_set_field(L, -1, "LOG_EMERG", LOG_EMERG);
+    luaX_set_field(L, -1, "LOG_ALERT", LOG_ALERT);
+    luaX_set_field(L, -1, "LOG_CRIT", LOG_CRIT);
+    luaX_set_field(L, -1, "LOG_ERR", LOG_ERR);
+    luaX_set_field(L, -1, "LOG_WARNING", LOG_WARNING);
+    luaX_set_field(L, -1, "LOG_NOTICE", LOG_NOTICE);
+    luaX_set_field(L, -1, "LOG_INFO", LOG_INFO);
+    luaX_set_field(L, -1, "LOG_DEBUG", LOG_DEBUG);
   }
 }
